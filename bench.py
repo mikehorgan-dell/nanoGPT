5d9f91c8d14756e8140ef0ba54b4c28ddf2dbfeb
Mike Horgan <mike@pocomotech.com>
2024-07-11 10:53:53 -0400

    Break out cuda_available test for easier debugging

--- bench.py
+++ bench.py
@@ -15,7 +15,9 @@ bias = False
 real_data = True
 seed = 1337
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
+cuda_available = torch.cuda.is_available()
+cuda_bf16 = cuda_available and torch.cuda.is_bf16_supported
+dtype = 'bfloat16' if cuda_available and cuda_bf16 else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = True # use PyTorch 2.0 to compile the model to be faster
 profile = False # use pytorch profiler, or just simple benchmarking?
 exec(open('configurator.py').read()) # overrides from command line or config file
